0e5976be610e726c19195612da9475f7ba2f9883
Unleashmarketing <unleashjonas@gmail.com>
2025-04-27 04:40:42 +0200

    Update custom (3).js

--- custom (3).js
+++ custom (3).js
@@ -9,3 +9,29 @@ window.addEventListener('load', function () {
       defaultDate: ["01-01-2025", "14-01-2025"],
     })
   })
+
+document.addEventListener('DOMContentLoaded', function() {
+  // Range Date Picker
+  if (document.getElementById('flatpickr-range')) {
+    flatpickr('#flatpickr-range', {
+      mode: 'range',
+      altInput: true,
+      altFormat: 'F j, Y',
+      dateFormat: 'd-m-Y',
+      locale: "de",
+      defaultDate: ["01-01-2025", "14-01-2025"],
+    });
+  }
+
+  // Single Date Picker
+  if (document.getElementById('flatpickr-date')) {
+    flatpickr('#flatpickr-date', {
+      altInput: true,
+      altFormat: 'F j, Y',
+      dateFormat: 'd-m-Y',
+      locale: "de",
+    });
+  }
+
+  // Initialize any other UI components here
+});
